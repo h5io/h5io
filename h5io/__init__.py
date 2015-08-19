927e8764b42dd3c71ed574fe670ec3e093c6aba3
Eric Larson <larson.eric.d@gmail.com>
2015-08-19 15:52:28 -0400

    FIX: Reorganize

--- h5io/__init__.py
+++ h5io/__init__.py
@@ -3,4 +3,4 @@
 
 __version__ = '0.1.dev0'
 
-from ._hdf5 import read_hdf5, write_hdf5, _TempDir, object_diff  # noqa, analysis:ignore
+from ._h5io import read_hdf5, write_hdf5, _TempDir, object_diff  # noqa, analysis:ignore
