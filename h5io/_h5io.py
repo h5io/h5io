db49191ba0bd5185a92a14ce7f2bca2df823f12d
Fede Raimondo <federaimondo@gmail.com>
2017-09-19 11:08:03 +0200

    Added support for numpy values (#24)

--- h5io/_h5io.py
+++ h5io/_h5io.py
@@ -151,8 +151,9 @@ def _triage_write(key, value, root, comp_kw, where,
         else:  # isinstance(value, float):
             title = 'float'
         _create_titled_dataset(root, key, title, np.atleast_1d(value))
-    elif isinstance(value, np.bool_):
-        _create_titled_dataset(root, key, 'np_bool_', np.atleast_1d(value))
+    elif isinstance(value, (np.integer, np.floating, np.bool_)):
+        title = 'np_{}'.format(value.__class__.__name__)
+        _create_titled_dataset(root, key, title, np.atleast_1d(value))
     elif isinstance(value, string_types):
         if isinstance(value, text_type):  # unicode
             value = np.fromstring(value.encode('utf-8'), np.uint8)
@@ -303,8 +304,10 @@ def _triage_read(node, slash='ignore'):
     elif type_str in ('int', 'float'):
         cast = int if type_str == 'int' else float
         data = cast(np.array(node)[0])
-    elif type_str == 'np_bool_':
-        data = np.bool_(np.array(node)[0])
+    elif type_str.startswith('np_'):
+        np_type = type_str.split('_')[1]
+        cast = getattr(np, np_type)
+        data = cast(np.array(node)[0])
     elif type_str in ('unicode', 'ascii', 'str'):  # 'str' for backward compat
         decoder = 'utf-8' if type_str == 'unicode' else 'ASCII'
         cast = text_type if type_str == 'unicode' else str
