1a1849dda03beb576f3a8189d0326d33c38a2377
Eric Larson <larson.eric.d@gmail.com>
2018-02-28 08:01:38 -0500

    FIX: Version bump

--- h5io/__init__.py
+++ h5io/__init__.py
@@ -1,7 +1,7 @@
 """Python Objects Onto HDF5
 """
 
-__version__ = '0.1.dev0'
+__version__ = '0.1.0'
 
 from ._h5io import (read_hdf5, write_hdf5, _TempDir,  # noqa, analysis:ignore
                     object_diff, list_file_contents)  # noqa, analysis:ignore
