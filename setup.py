df75fb8ecd765229a699054f2e6982b1fd5e137c
Jan Janssen <jan.janssen@outlook.com>
2018-08-13 21:57:24 +0200

    Add versioneer

--- setup.py
+++ setup.py
@@ -8,15 +8,7 @@ import os
 
 import setuptools  # noqa; we are using a setuptools namespace
 from numpy.distutils.core import setup
-
-version = None
-with open(os.path.join('h5io', '__init__.py'), 'r') as fid:
-    for line in (line.strip() for line in fid):
-        if line.startswith('__version__'):
-            version = line.split('=')[1].strip().strip('\'')
-            break
-if version is None:
-    raise RuntimeError('Could not determine version')
+import versioneer
 
 
 descr = """Python Objects Onto HDF5"""
@@ -28,7 +20,6 @@ MAINTAINER_EMAIL = 'larson.eric.d@gmail.com'
 URL = 'http://h5io.github.io'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'http://github.com/h5io/h5io'
-VERSION = version
 
 
 if __name__ == "__main__":
@@ -42,7 +33,7 @@ if __name__ == "__main__":
           description=DESCRIPTION,
           license=LICENSE,
           url=URL,
-          version=VERSION,
+          version=versioneer.get_version(),
           download_url=DOWNLOAD_URL,
           long_description=open('README.rst').read(),
           zip_safe=False,  # the package can run out of an .egg file
@@ -59,4 +50,5 @@ if __name__ == "__main__":
           platforms='any',
           packages=['h5io', 'h5io.tests'],
           package_data={},
-          scripts=[])
+          scripts=[],
+          cmdclass=versioneer.get_cmdclass())
