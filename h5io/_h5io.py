3a823826032167c73efe88ac8ed080ced20a8e71
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-08-13 21:22:30 +0200

    Improve performance by storing dictionaries and lists as JSON objects (#31)
    * json
    
    * use_json=False
    
    https://github.com/h5io/h5io/pull/31#issuecomment-412363759
    
    * Add tests plus bug fix
    
    * falke fixes
    
    * Add Docstring
    
    * Remove tailing white spaces
    
    * Test more types within the dictionary
    
    * Fix tests
    
    * Fixed bug
    
    * Remove tailing whitespace
    
    * Add test for dictionary with numpy element
    
    this can not be stored in JSON format.
    
    * Remove tailing whitespace
    
    * Convert dict_values to list for comparison
    
    * Update test_io.py
    
    * Test dict keys with slash
    
    * Support unicode strings in Python 2.X

--- h5io/_h5io.py
+++ h5io/_h5io.py
@@ -3,6 +3,7 @@
 #
 # License: BSD (3-clause)
 
+import json
 import sys
 import tempfile
 from shutil import rmtree
@@ -59,7 +60,7 @@ def _create_pandas_dataset(fname, root, key, title, data):
 
 
 def write_hdf5(fname, data, overwrite=False, compression=4,
-               title='h5io', slash='error'):
+               title='h5io', slash='error', use_json=False):
     """Write python object to HDF5 format using h5py
 
     Parameters
@@ -83,6 +84,9 @@ def write_hdf5(fname, data, overwrite=False, compression=4,
         Whether to replace forward-slashes ('/') in any key found nested within
         keys in data. This does not apply to the top level name (title).
         If 'error', '/' is not allowed in any lower-level keys.
+    use_json : bool
+        To accelerate the read and write perfromance of small dictionaries and
+        lists they can be combined to JSON objects and stored as strings.
     """
     h5py = _check_h5py()
     mode = 'w'
@@ -104,7 +108,8 @@ def write_hdf5(fname, data, overwrite=False, compression=4,
             del fid[title]
         cleanup_data = []
         _triage_write(title, data, fid, comp_kw, str(type(data)),
-                      cleanup_data=cleanup_data, slash=slash, title=title)
+                      cleanup_data=cleanup_data, slash=slash, title=title,
+                      use_json=use_json)
 
     # Will not be empty if any extra data to be written
     for data in cleanup_data:
@@ -116,7 +121,7 @@ def write_hdf5(fname, data, overwrite=False, compression=4,
 
 
 def _triage_write(key, value, root, comp_kw, where,
-                  cleanup_data=[], slash='error', title=None):
+                  cleanup_data=[], slash='error', title=None, use_json=False):
     if key != title and '/' in key:
         if slash == 'error':
             raise ValueError('Found a key with "/", '
@@ -128,7 +133,11 @@ def _triage_write(key, value, root, comp_kw, where,
         else:
             raise ValueError("slash must be one of ['error', 'replace'")
 
-    if isinstance(value, dict):
+    if use_json and isinstance(value, (list, dict)) and \
+            json_compatible(value, slash=slash):
+        value = np.frombuffer(json.dumps(value).encode('utf-8'), np.uint8)
+        _create_titled_dataset(root, key, 'json', value, comp_kw)
+    elif isinstance(value, dict):
         sub_root = _create_titled_group(root, key, 'dict')
         for key, sub_value in value.items():
             if not isinstance(key, string_types):
@@ -323,6 +332,9 @@ def _triage_read(node, slash='ignore'):
         decoder = 'utf-8' if type_str == 'unicode' else 'ASCII'
         cast = text_type if type_str == 'unicode' else str
         data = cast(np.array(node).tostring().decode(decoder))
+    elif type_str == 'json':
+        node_unicode = str(np.array(node).tostring().decode('utf-8'))
+        data = json.loads(node_unicode)
     elif type_str == 'None':
         data = None
     else:
@@ -498,6 +510,34 @@ def list_file_contents(h5file):
         if not isinstance(h5file, h5py.File):
             raise TypeError(err.format(type(h5file)))
         _list_file_contents(h5file)
+
+
+def json_compatible(obj, slash='error'):
+    if isinstance(obj, (string_types, int, float, bool, type(None))):
+        return True
+    elif isinstance(obj, list):
+        return all([json_compatible(item) for item in obj])
+    elif isinstance(obj, dict):
+        _check_keys_in_dict(obj, slash=slash)
+        return all([json_compatible(item) for item in obj.values()])
+    else:
+        return False
+
+
+def _check_keys_in_dict(obj, slash='error'):
+    for key in obj.keys():
+        if '/' in key:
+            key_prev = key
+            if slash == 'error':
+                raise ValueError('Found a key with "/", '
+                                 'this is not allowed if slash == error')
+            elif slash == 'replace':
+                # Auto-replace keys with proper values
+                for key_spec, val_spec in special_chars.items():
+                    key = key.replace(val_spec, key_spec)
+                obj[key] = obj.pop(key_prev)
+            else:
+                raise ValueError("slash must be one of ['error', 'replace'")
 
 
 ##############################################################################
