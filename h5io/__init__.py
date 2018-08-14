f9841d916066c78052263f3659fbac2790998775
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-08-14 14:54:49 +0200

    Versioneer (#33)
    * Add pip deploy to travis config
    
    * Add versioneer
    
    * Add versioneer
    
    * Exclude Versioneer from the test coverage
    
    * Add pypi token
    
    * Set token as global variable

--- h5io/__init__.py
+++ h5io/__init__.py
@@ -1,7 +1,10 @@
 """Python Objects Onto HDF5
 """
 
-__version__ = '0.1.1.dev0'
 
 from ._h5io import (read_hdf5, write_hdf5, _TempDir,  # noqa, analysis:ignore
                     object_diff, list_file_contents)  # noqa, analysis:ignore
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
